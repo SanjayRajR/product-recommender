99e0903c113c535e8108afc6ec947af39a18e477
Anu-Nair <43957718+Anu-Nair@users.noreply.github.com>
2020-07-17 15:38:56 +0530

    first

---
+++ main/accounts/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('login', views.login, name='login'),
+    path('register', views.register, name='register'),
+    path('logout', views.logout, name='logout'),
+    path('dashboard', views.dashboard, name='dashboard'),
+]
+
+# for the path localhost:8000/accounts/login
+# for the path localhost:8000/accounts/register and so on
