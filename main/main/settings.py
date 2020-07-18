8a6b1d22a44aaa2ef507fecab5ac306cbc9774fb
Anu-Nair <43957718+Anu-Nair@users.noreply.github.com>
2020-07-19 00:27:58 +0530

    working db

--- main/main/settings.py
+++ main/main/settings.py
@@ -80,11 +80,13 @@ WSGI_APPLICATION = 'main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'frontend',
+        'USER': 'postgres',
+        'PASSWORD': '1234' ,
+        'HOST':'localhost'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
